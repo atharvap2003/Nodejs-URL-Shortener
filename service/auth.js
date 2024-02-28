6280bea618ce4ad2d0dfce6e0fd1915a70f2465e
Atharva Pandharikar <atharvapandharikar5@gmail.com>
2024-02-28 17:17:45 +0530

    jwt added

--- service/auth.js
+++ service/auth.js
@@ -1,14 +1,27 @@
-const sessionIdUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Piyush$123@$";
 
-function setUser(id, user){
-    sessionIdUserMap.set(id, user);
-}
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email:user.email,
+    },
+    secret
+  );
+};
 
-function getUser(id){
-    return sessionIdUserMap.get(id);
-}
+function getUser(token) {
+  if (!token) return null;
+  try{
+      return jwt.verify(token, secret);
+  }
+  catch(error){
+    return null;
+  }
+};
 
 module.exports = {
-    setUser,
-    getUser
+  setUser,
+  getUser,
 };
